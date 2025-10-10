d9b8e2fb058f195ac8807cba284f806542b83212
omkanekar28 <omkanekar28@gmail.com>
2025-10-10 10:55:25 +0000

    Added .doc support via UnstructuredWordDocumentLoader

--- src/ingestion/doc_processor.py
+++ src/ingestion/doc_processor.py
@@ -1,7 +1,7 @@
 import time
 from typing import List
 from abc import ABC, abstractmethod
-from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import Docx2txtLoader, UnstructuredWordDocumentLoader
 from ..utils.logger_setup import setup_logger
 from ..utils.common import validate_file_location, validate_file_type
 
@@ -31,8 +31,8 @@ class BaseDocProcessor(ABC):
         pass
 
 
-class Doc2TxtProcessor(BaseDocProcessor):
-    """Class for Document processing using Docx2txt library"""
+class Doc2TxtDocProcessor(BaseDocProcessor):
+    """Class for Document processing using Docx2txt library (.doc not supported)"""
 
     def __init__(self, filepath: str) -> None:
         """
@@ -42,6 +42,10 @@ class Doc2TxtProcessor(BaseDocProcessor):
             filepath (str): Path to the Document file
         """
         try:
+            # THROW ERROR IF .doc FILE IS PROVIDED
+            if filepath.lower().endswith(".doc"):
+                raise ValueError("Docx2txtLoader does not support .doc files. Please use UnstructuredDocProcessor for .doc files.")
+            
             super().__init__(filepath)
             self.loader = Docx2txtLoader(file_path=self.filepath)
         except Exception as e:
@@ -52,7 +56,7 @@ class Doc2TxtProcessor(BaseDocProcessor):
         """Processes the Document and returns text found"""
         try:
             processing_start_time = time.time()
-            logger.info(f"Processing file: '{self.filepath}' using PyMuPDF4LLM...")
+            logger.info(f"Processing file: '{self.filepath}' using Doc2Txt...")
             documents = self.loader.load()
             if not documents or not documents[0].page_content.strip():
                 logger.warning("No text found in the document.")
@@ -66,9 +70,44 @@ class Doc2TxtProcessor(BaseDocProcessor):
             raise RuntimeError(f"Failed to process '{self.filepath}' document using Doc2Txt! {e}")
 
 
+class UnstructuredDocProcessor(BaseDocProcessor):
+    """Class for Document processing using Docx2txt library"""
+
+    def __init__(self, filepath: str) -> None:
+        """
+        Initializes the Unstructured Loader
+
+        Args:
+            filepath (str): Path to the Document file
+        """
+        try:
+            super().__init__(filepath)
+            self.loader = UnstructuredWordDocumentLoader(file_path=self.filepath)
+        except Exception as e:
+            logger.error(f"Error initializing UnstructuredWordDocumentLoader: {e}")
+            raise
+
+    def process(self) -> List[str]:
+        """Processes the Document and returns text found"""
+        try:
+            processing_start_time = time.time()
+            logger.info(f"Processing file: '{self.filepath}' using UnstructuredDocProcessor...")
+            documents = self.loader.load()
+            if not documents or not documents[0].page_content.strip():
+                logger.warning("No text found in the document.")
+                return []
+            logger.info(
+                f"File: '{self.filepath}' processed in "
+                f"{time.time() - processing_start_time:.2f} seconds"
+            )
+            return [documents[0].page_content.strip()]
+        except Exception as e:
+            raise RuntimeError(f"Failed to process '{self.filepath}' document using UnstructuredWordDocumentLoader! {e}")
+
+
 # EXAMPLE USAGE
 # if __name__ == "__main__":
-#     processor = Doc2TxtProcessor(
+#     processor = UnstructuredDocProcessor(
 #         filepath="",
 #     )
 #     results = processor.process()
