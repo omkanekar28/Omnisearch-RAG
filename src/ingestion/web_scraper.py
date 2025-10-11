2af3f969597013010c62e8517f0940ae761b9350
omkanekar28 <omkanekar28@gmail.com>
2025-10-11 07:02:03 +0000

    Added logging for web scraper

--- src/ingestion/web_scraper.py
+++ src/ingestion/web_scraper.py
@@ -1,5 +1,9 @@
+import time
 import requests
 from bs4 import BeautifulSoup
+from ..utils.logger_setup import setup_logger
+
+logger = setup_logger("web_scraper.py")
 
 
 class WebScraper:
@@ -13,6 +17,8 @@ class WebScraper:
 
     def __call__(self, url: str) -> str:
         """Fetches and returns the main textual content of a given URL."""
+        processing_start_time = time.time()
+        logger.info(f"Scraping URL: '{url}' ...")
         try:
             response = requests.get(url, timeout=self.request_timeout)
             response.raise_for_status()    # RAISES HTTPError FOR BAD RESPONSES
@@ -20,15 +26,16 @@ class WebScraper:
             return f"Error fetching URL: {e}"
 
         soup = BeautifulSoup(response.text, "html.parser")
-
-        print(len(soup.get_text(separator="\n", strip=True)))
-
         text = soup.get_text(separator="\n", strip=True)
+        logger.info(
+            f"URL: '{url}' processed in "
+            f"{time.time() - processing_start_time:.2f} seconds"
+        )
         return text
 
 
-# if __name__ == "__main__":
-#     scraper = WebScraper()
-#     url = ""
-#     result = scraper(url)
-#     print(result)
+if __name__ == "__main__":
+    scraper = WebScraper()
+    url = "https://oneshield.com/about/"
+    result = scraper(url)
+    print(result)
