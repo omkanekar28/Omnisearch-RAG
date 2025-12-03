be72682d6a963de08aaca79a28710ae90c2d434f
omkanekar28 <omkanekar28@gmail.com>
2025-12-03 11:41:36 +0000

    Code removal

--- src/utils/logger_setup.py
+++ src/utils/logger_setup.py
@@ -16,29 +16,24 @@ def setup_logger(
         logging.Logger: Configured logger instance.
     """
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)    # Capture all levels internally
+    logger.setLevel(logging.DEBUG)
 
-    # Prevent duplicate logs if function is called multiple times
     if logger.handlers:
         return logger
 
-    # --- Formatter ---
     formatter = logging.Formatter(
         fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
 
-    # --- Console Handler ---
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter)
 
-    # --- File Handler ---
     file_handler = logging.FileHandler(filename, mode='a', encoding='utf-8')
     file_handler.setLevel(logging.INFO)  # Skip DEBUG logs
     file_handler.setFormatter(formatter)
 
-    # --- Add Handlers ---
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
